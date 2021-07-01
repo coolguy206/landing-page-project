906b7bd849efaea00ba54e7cf8656b8f69e70963
David Thanphilom <davidthanphilom@msn.com>
2021-06-30 18:58:17 -0700

    setup complete

---
+++ grunt/includes.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  options: {
+    flatten: true
+  },
+
+  dist: {
+    files: [{
+      cwd: 'html/',
+      src: ['index.html'],
+      dest: '',
+    }, ],
+  },
+
+};
---
+++ grunt/purifycss.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  options: {
+    //sourceMap:true,
+  },
+
+  dist: {
+    src: ['index.html'],
+    css: ['css/styles.css'],
+    dest: 'css/purify/styles.css'
+  },
+
+
+};
