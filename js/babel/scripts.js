74b549146b98afc32dea823c17237440ba2b03c1
David Thanphilom <davidthanphilom@msn.com>
2021-07-12 16:18:26 -0700

    coded sticky header, overlay, & error inputs master

--- js/babel/scripts.js
+++ js/babel/scripts.js
@@ -2,4 +2,11 @@
 
 $(document).ready(function () {
   console.log('ready');
+  $(window).on('scroll', function (e) {
+    if (window.pageYOffset > 98) {
+      $('.wrapper header').addClass('sticky');
+    } else {
+      $('.wrapper header').removeClass('sticky');
+    }
+  });
 });
--- js/browserify/scripts.js
+++ js/browserify/scripts.js
@@ -3,6 +3,13 @@
 
 $(document).ready(function () {
   console.log('ready');
+  $(window).on('scroll', function (e) {
+    if (window.pageYOffset > 98) {
+      $('.wrapper header').addClass('sticky');
+    } else {
+      $('.wrapper header').removeClass('sticky');
+    }
+  });
 });
 
 },{}]},{},[1]);
