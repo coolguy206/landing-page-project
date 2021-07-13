f7705453e493aefae28e9b26d2158c79c26d4e65
David Thanphilom <davidthanphilom@msn.com>
2021-07-13 11:56:03 -0700

    setup complete test

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
   console.log('ready');
-
-  $(window).on('scroll', function(e) {
-
-    if (window.pageYOffset > 98) {
-      $('.wrapper header').addClass('sticky');
-    } else {
-      $('.wrapper header').removeClass('sticky');
-    }
-  });
 });
